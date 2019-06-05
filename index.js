e177ab2f21abdd4e5b2ba5a2842a003059145d02
jedspears <jed.spears7@gmail.com>
2019-06-05 10:03:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
@@ -9,6 +11,11 @@ function renderBooks(json) {
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
   })
+  const baba = document.createElement('h2')
+  baba.innerHTML = `<h2>${json[4].name}!!!</h2>`
+
+  const gaga = document.createElement('h2')
+  gaga.innerHTML = `<h2>${json[4].name}!!!</h2>`
 }
 
 document.addEventListener('DOMContentLoaded', function() {
