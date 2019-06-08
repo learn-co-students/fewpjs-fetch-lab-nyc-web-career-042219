f6135df166d7f39da0e4ed7d70b9da4612883720
Tim Koar <koartimothy@gmail.com>
2019-06-08 17:18:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-function fetchBooks() {
-
+function fetchBooks(){
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(response => response.json())
+  .then(json => renderBooks(json))
 }
-
 function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
+    main.appendChild(h2);
   })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function(){
   fetchBooks()
 })
